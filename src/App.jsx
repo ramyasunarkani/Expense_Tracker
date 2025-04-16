ceb7718d3cefd4517483f3f829d5b8b75af0837a
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-04-16 15:07:36 +0530

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,50 @@
-import './App.css'
-import React from 'react';
-
-import Home from './Components/Expenses/Home';
+import './App.css';
+import React, { Suspense, lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import CompleteProfile from './Components/Profile/CompleteProfile';
-import ForgotPassword from './Components/Authentication/ForgotPassword';
 import { useSelector } from 'react-redux';
-import Login from './Components/Authentication/Login';
-import SignUp from './Components/Authentication/SignUp';
-import { useEffect } from 'react';
+
+const Home = lazy(() => import('./Components/Expenses/Home'));
+const CompleteProfile = lazy(() => import('./Components/Profile/CompleteProfile'));
+const ForgotPassword = lazy(() => import('./Components/Authentication/ForgotPassword'));
+const Login = lazy(() => import('./Components/Authentication/Login'));
+const SignUp = lazy(() => import('./Components/Authentication/SignUp'));
 
 function App() {
   const userLogged = useSelector(state => state.auth.isLoggedIn);
   const mode = useSelector(state => state.theme.mode);
 
-useEffect(() => {
-  document.body.className = mode; 
-}, [mode]);
+  useEffect(() => {
+    document.body.className = mode; 
+  }, [mode]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to={userLogged ? "/home" : "/login"} />} />
-      
-      <Route 
-        path="/login" 
-        element={userLogged ? <Navigate to="/home" /> : <Login />} 
-      />
-      
-      <Route 
-        path="/signUp" 
-        element={userLogged ? <Navigate to="/home" /> : <SignUp />} 
-      />
+    <Suspense fallback={<p>Loading...</p>}>
+      <Routes>
+        <Route path="/" element={<Navigate to={userLogged ? "/home" : "/login"} />} />
+        
+        <Route 
+          path="/login" 
+          element={userLogged ? <Navigate to="/home" /> : <Login />} 
+        />
+        
+        <Route 
+          path="/signUp" 
+          element={userLogged ? <Navigate to="/home" /> : <SignUp />} 
+        />
 
-      <Route 
-        path="/home" 
-        element={userLogged ? <Home /> : <Navigate to="/login" />} 
-      />
+        <Route 
+          path="/home" 
+          element={userLogged ? <Home /> : <Navigate to="/login" />} 
+        />
 
-      <Route 
-        path="/profile" 
-        element={userLogged ? <CompleteProfile /> : <Navigate to="/login" />} 
-      />
+        <Route 
+          path="/profile" 
+          element={userLogged ? <CompleteProfile /> : <Navigate to="/login" />} 
+        />
 
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-    </Routes>
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Suspense>
   );
 }
 
